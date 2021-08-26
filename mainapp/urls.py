e85718fbccf5228058f33d6c6b35e7bf224f58af
leonardar <leonardar88@gmail.com>
2021-08-26 13:15:23 +0300

    working CBV products

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from mainapp.views import products
+from mainapp.views import ProductView
 
 ap_name = 'mainapp'
 
-urlpatterns = [
-    path ('', products, name='index'),
-    path ('<int:category_id>/', products, name='product'),
-    path ('page/<int:page>/', products, name='page')
-]
+# urlpatterns = [
+#     path ('', ProductView.as_view(), name='index'),
+#     path ('<int:category_id>/', ProductView.as_view(), name='product'),
+#     path ('page/<int:page>/<int:category_id>/', ProductView.as_view(), name='page'),
+#     path ('page/<int:page>/', ProductView.as_view(), name='page'),
+#     path ('categories/', ProductCategoryView.as_view(), name='categories')
+# ]
 
+urlpatterns = [
+    path ('', ProductView.as_view(), name='index'),
+    path ('<int:category_id>/', ProductView.as_view(), name='product'),
+    path ('page/<int:page>/', ProductView.as_view(), name='page')
+]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,80 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from mainapp.models import Product, ProductCategory
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from basketapp.models import Basket
-from django.core.cache import cache
-from django.conf import settings
-
-def get_links_menu():
-   if settings.LOW_CACHE:
-       key = 'links_menu'
-       links_menu = cache.get(key)
-       if links_menu is None:
-           links_menu = ProductCategory.objects.all()
-           cache.set(key, links_menu)
-       return links_menu
-   else:
-       return ProductCategory.objects.all()
-
-def get_category(pk):
-   if settings.LOW_CACHE:
-       key = f'category_{pk}'
-       category = cache.get(key)
-       if category is None:
-           category = get_object_or_404(ProductCategory, pk=pk)
-           cache.set(key, category)
-       return category
-   else:
-       return get_object_or_404(ProductCategory, pk=pk)
-
-def get_products(category_id):
-   if settings.LOW_CACHE:
-       key = 'products'
-       products = cache.get(key)
-       if products is None:
-           products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-           cache.set(key, products)
-       return products
-   else:
-       return Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-
-def get_product(pk):
-   if settings.LOW_CACHE:
-       key = f'product_{pk}'
-       product = cache.get(key)
-       if product is None:
-           product = get_object_or_404(Product, pk=pk)
-           cache.set(key, product)
-       return product
-   else:
-       return get_object_or_404(Product, pk=pk)
-
-def get_products_orederd_by_price():
-   if settings.LOW_CACHE:
-       key = 'products_orederd_by_price'
-       products = cache.get(key)
-       if products is None:
-           products = Product.objects.filter(is_active=True, \
-                                  category__is_active=True).order_by('price')
-           cache.set(key, products)
-       return products
-   else:
-       return Product.objects.filter(is_active=True,\
-                                 category__is_active=True).order_by('price')
 
 
-def get_products_in_category_orederd_by_price(pk):
-   if settings.LOW_CACHE:
-       key = f'products_in_category_orederd_by_price_{pk}'
-       products = cache.get(key)
-       if products is None:
-           products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
-           cache.set(key, products)
-       return products
-   else:
-       return Product.objects.filter(category__pk=pk, is_active=True,category__is_active=True).order_by('price')
-
-
+# Create your views here.
 def index(request):
     if request.user.is_authenticated:
         baskets = Basket.objects.filter(user=request.user)
@@ -87,25 +19,67 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, category_id=None,page=1):
-    if request.user.is_authenticated:
-        baskets = Basket.objects.filter(user=request.user)
-        context = {
-        'title': 'Geekshop - Каталог',
-        'categories': get_links_menu(),
-        'baskets': baskets}
-    else:
-        context = {'title': 'Geekshop - Каталог',
-                'categories': get_links_menu()}
+# def products(request, category_id=None,page=1):
+#     if request.user.is_authenticated:
+#         baskets = Basket.objects.filter(user=request.user)
+#         context = {
+#         'title': 'Geekshop - Каталог',
+#         'categories': ProductCategory.objects.all(),
+#         'baskets': baskets}
+#     else:
+#         context = {'title': 'Geekshop - Каталог',
+#                 'categories': ProductCategory.objects.all()}
+#
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     paginator = Paginator(products, per_page=3)
+#     try:
+#         products_paginator = paginator.page(page)
+#     except PageNotAnInteger:
+#         products_paginator = paginator.page(1)
+#     except EmptyPage:
+#         products_paginator = paginator.page(paginator.num_pages)
+#     context.update({'products': products_paginator})
+#     return render(request, 'mainapp/products.html', context)
 
-    products = get_products(category_id)
-    paginator = Paginator(products, per_page=4)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-    context.update({'products': products_paginator})
-    return render(request, 'mainapp/products.html', context)
+class ProductView(ListView):
+    model = Product
+    template_name = 'mainapp/products.html'
+    # paginator = Paginator
+    # paginate_by = 4
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        if category_id == None:
+            products = Product.objects.all()
+        else:
+            products = Product.objects.filter(category_id=category_id)
+        categories = ProductCategory.objects.all()
+        queryset = {'products': products, 'categories': categories }
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductView, self).get_context_data(**kwargs)
+        # category_id = kwargs.get('category_id', None)
+        # page = kwargs.get('page', None)
+
+        # paginator = self.paginator(products, per_page=3)
+        # try:
+        #     products_paginator = paginator.page(page)
+        # except PageNotAnInteger:
+        #     products_paginator = paginator.page(1)
+        # except EmptyPage:
+        #     products_paginator = paginator.page(paginator.num_pages)
+        # products = paginator.page(page)
+        context.update({'title': 'Geekshop - Каталог', 'categories': self.get_queryset()['categories'] ,'products': self.get_queryset()['products']})
+        return context
+
+
+class ProductCategoryView(ListView):
+    model = ProductCategory
+    template_name = 'mainapp/products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductCategoryView, self).get_context_data(**kwargs)
+        context.update({'title': 'Geekshop - Каталог', 'categories': ProductCategory.objects.all()})
+        return context
 
